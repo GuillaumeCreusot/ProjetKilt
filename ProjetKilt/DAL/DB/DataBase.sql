9c425548e26df225c3d5ed440c445829746a1c41
Guillaume Creusot <gcreusot@ensc.fr>
2018-11-12 11:25:12 +0100

    Add SQL scripts

---
+++ ProjetKilt/DAL/DB/DataBase.sql
@@ -0,0 +1,4 @@
+﻿create database if not exists kilt character set utf8 collate utf8_unicode_ci;
+use kilt;
+
+grant all privileges on kilt.* to 'thierry'@'localhost' identified by 'kilt';
---
+++ ProjetKilt/DAL/DB/Structure.sql
@@ -0,0 +1,52 @@
+﻿drop table if exists courreur;
+drop table if exists course;
+drop table if exists participation;
+drop table if exists sponsorise;
+drop table if exists sponsor;
+drop table if exists utilisateur;
+
+
+create table utilisateur (
+    id integer not null primary key auto_increment,
+    nom varchar(100) not null,
+    mdp_hash int not null
+);
+
+create table coureur (
+    id integer not null primary key auto_increment,
+    nom varchar(100) not null,
+	prenom varchar(100) not null,
+	mail varchar(100) not null,
+	sexe varchar(100) not null,
+	licence_ffa varchar(100) not null,
+    date_naissance datetime not null
+);
+
+create table course (
+	id integer not null primary key auto_increment,
+	nom varchar(100) not null,
+	kilometrage double not null
+);
+
+create table sponsor (
+	id integer not null primary key auto_increment,
+	nom_sponsor string not null
+);
+
+create table participation (
+	id integer not null primary key auto_increment,
+	num_dossard int not null,
+	temps int not null,
+	coureur_id int null,
+	course_id int null,
+	constraint fk_coureur_participation foreign key(coureur_id) references coureur(id),
+	constraint fk_course_participation foreign key(course_id) references course(id)
+);
+
+create table sponsorise (
+	id integer not null primary key auto_increment,
+	coureur_id int null,
+	sponsor_id int null,
+	constraint fk_coureur_sponsorise foreign key(coureur_id) references coureur(id),
+	constraint fk_sponsor_sponsorise foreign key(sponsor_id) references sponsor(id)
+);
