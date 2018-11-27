f3ee0dd90f65f87de82e504bcbc3e6db6642c0be
Louis Hache <lhache@ensc.fr>
2018-11-27 14:11:48 +0100

    Repos Test

---
+++ ProjetKilt/DAL/ReposTest/ParticipationRepoTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace DAL
+{
+    public class ParticipationRepoTest : Repository, IParticipationRepository
+    {
+
+        public List<Participation> ParticipationCollection;
+
+        public ParticipationRepoTest()
+        {
+            ParticipationCollection = new List<Participation>();
+
+            Coureur coureur1 = new Coureur("coureur1", "1", "XXXXXXXXX", "XXX@gmail.com", "X", DateTime.Today);
+            Coureur coureur2 = new Coureur("coureur2", "2", "XXXXXXXXX", "XXX@gmail.com", "X", DateTime.Today);
+            Coureur coureur3 = new Coureur("coureur3", "3", "XXXXXXXXX", "XXX@gmail.com", "X", DateTime.Today);
+
+            Course course1 = new Course("course1", 1);
+            Course course2 = new Course("course2", 2);
+
+            Participation participation1 = new Participation(coureur1, course1, 042, DateTime.Now);
+            ParticipationCollection.Add(participation1);
+            Participation participation2 = new Participation(coureur2, course1, 240, DateTime.Now);
+            ParticipationCollection.Add(participation2);
+            Participation participation3 = new Participation(coureur1, course2, 420, DateTime.Now);
+            ParticipationCollection.Add(participation3);
+        }
+
+
+
+        public Participation[] GetAll()
+        {
+
+            return ParticipationCollection.ToArray();
+
+        }
+
+        public void Save(Participation participation)
+        {
+            ParticipationCollection.Add(participation);
+        }
+    }
+}
