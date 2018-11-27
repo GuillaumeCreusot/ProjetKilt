1521112033c79c2b517c19ca94e4d651cd03566a
Louis Hache <lhache@ensc.fr>
2018-11-27 13:25:06 +0100

    IRepos et Repos (non testés)

---
+++ ProjetKilt/DAL/CourseRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace DAL
+{
+    public class CourseRepository : Repository, ICourseRepository
+    {
+        public Course[] GetAll()
+        {
+            return Session.Query<Course>().ToArray();
+        }
+
+        public void Save(Course course)
+        {
+            Session.SaveOrUpdate(course);
+            Session.Flush();
+        }
+    }
+}
---
+++ ProjetKilt/DAL/ParticipationRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace DAL
+{
+    public class ParticipationRepository : Repository, IParticipationRepository
+    {
+        public Participation[] GetAll()
+        {
+            return Session.Query<Participation>().ToArray();
+        }
+
+        public void Save(Participation part)
+        {
+            Session.SaveOrUpdate(part);
+            Session.Flush();
+        }
+    }
+}
