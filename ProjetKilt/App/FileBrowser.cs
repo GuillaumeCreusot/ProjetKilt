031728d50a686b3c446cac074255d1b5e9efbf6c
GuillaumeCreusot <gcreusot@ensc.fr>
2018-12-18 08:57:42 +0100

    CSV handler WIP

---
+++ ProjetKilt/App/FileBrowser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace App
+{
+    class FileBrowser
+    {
+        public static FileInfo GetFileByBrowser(string initDirPath, string contentName, string extension = "txt")
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = $"Sélectionnez votre fichier contenant {contentName}.";
+            op.InitialDirectory = initDirPath;
+            op.Multiselect = false;
+            op.Filter = $"Question File|*.{extension}";
+            if (op.ShowDialog() == DialogResult.OK)
+            {
+                return new FileInfo(op.FileName);
+            }
+            return null;
+
+        }
+    }
+}
