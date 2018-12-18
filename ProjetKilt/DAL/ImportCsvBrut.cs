131f29339fb4627106b9844fd3d5ee433547fdf5
Louis Hache <lhache@ensc.fr>
2018-12-18 09:00:54 +0100

    Import CSV to Dico

---
+++ ProjetKilt/DAL/ImportCsvBrut.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DAL
+{
+    public class ImportCsvBrut
+    {
+
+        public static Dictionary<string, List<string>> ImportCsv(string path)
+        {
+            Dictionary<string, List<string>> table = new Dictionary<string, List<string>>();
+            StreamReader reader = new StreamReader(path);
+
+            string line = reader.ReadLine();
+            List<string> keys = line.Split(',').ToList();
+
+
+            foreach (string key in keys)
+            {
+                table.Add(key, new List<string>());
+            }
+
+
+            while (!reader.EndOfStream)
+            {
+                line = reader.ReadLine();
+                List<string> values = line.Split(',').ToList();
+
+                for (int i = 0; i < values.Count(); i++)
+                    table[keys[i]].Add(values[i]);
+
+            }
+
+            return table;
+        }
+    }
+}
